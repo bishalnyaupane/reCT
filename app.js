5e16c3b4dba24f2bacfd287c089d386e9d58006e
Bishal Nyaupane <160811469+bishalnyaupane@users.noreply.github.com>
2024-05-29 12:44:58 +0545

    Update

---
+++ app.js
@@ -0,0 +1,34 @@
+// let star = ""
+// for (let i = 5; i > 0; i--) {
+
+//     for (let k = 0; k < i; k++) {
+//         star += " "
+//     }
+//     for (let j = 1; j < 5 - i; j++) {
+//         star += "*"
+//     }
+//     star += '\n'
+// }
+// console.log(star)
+// let n = 5
+// let star = ""
+// while (n > 0) {
+//     let c = 0;
+//     while (c < 5) {
+//         star += "*"
+//         c++
+//     }
+//     star += "\n"
+//     n--;
+// }
+// console.log(star)
+// do{
+//     console.log(n)
+//     n--
+// } while(n>6)
+// function
+let buttonname = document.getElementById('button')
+
+function clickme() {
+    buttonname.style.transform = 'translateY(555%)'
+}
